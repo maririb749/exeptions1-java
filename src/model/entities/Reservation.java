7a2385c7a0bda8d4fc11ddd7d2a799a20d9a8915
Mariana Santos <maririb51@gmail.com>
2022-08-02 12:54:25 +0100

    criando exceções personalizadas(terceira solucao boa

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -13,6 +15,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException(" Ckeck-out date must be after ckeck-in date!");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkin;
 		this.checkOut = checkout;
@@ -40,17 +45,17 @@ public class Reservation {
 
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut)  {
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return " Reservation dates for update must be future dates";
-		} 
+			throw new DomainException(" Reservation dates for update must be future dates");
+		}
 		if (!checkOut.after(checkIn)) {
-			return " Ckeck-out date must be after ckeck-in date!";
+			throw new DomainException(" Ckeck-out date must be after ckeck-in date!");
 		}
 		this.checkIn = checkIn;
 		this.checkIn = checkOut;
-		return null;
+
 	}
 
 	@Override
